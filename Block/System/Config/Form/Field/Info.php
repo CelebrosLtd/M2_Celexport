79053602cb8f9b28189ca1e789220a62884fa44a
Devbelvg <devbelvg@gmail.com>
2015-12-21 13:10:06 +0100

    dev update

--- Block/System/Config/Form/Field/Info.php
+++ Block/System/Config/Form/Field/Info.php
@@ -17,25 +17,17 @@ use Magento\Framework\Setup\ModuleContextInterface;
 class Info extends \Magento\Config\Block\System\Config\Form\Field
 {
     const MODULE_NAME = 'Celebros_Celexport';
-    protected $_helper;
-    protected $_module;
     protected $_moduleDb;
     
     public function __construct(
-        \Celebros\Celexport\Helper\Data $helper,
         \Magento\Framework\Module\ResourceInterface $moduleDb
     ) {
-        $this->_helper = $helper;
         $this->_moduleDb = $moduleDb;
     }
     
     public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
         $id = $element->getHtmlId();
-        /*$envStamp = $this->_helper->getCurrentEnvStamp();
-        $html = '<tr id="row_' . $id . '">';
-        $html .= '<td class="label" colspan="1">' . __('Current Env Stamp') . '</td><td class="value" colspan="4">' . $envStamp . '</td>';
-        $html .= '</tr>';*/
         $html = '<tr id="row_' . $id . '">';
         $html .= '<td class="label">' . __('Module Version') . '</td><td class="value">' . $this->getModuleVersion() . '</td><td class="scope-label"></td>';
         $html .= '</tr>';
@@ -45,7 +37,7 @@ class Info extends \Magento\Config\Block\System\Config\Form\Field
     
     public function getModuleVersion()
     {
-        return $this->_moduleDb->getDbVersion('Celebros_Celexport');
+        return $this->_moduleDb->getDbVersion(self::MODULE_NAME);
     }
     
 }
