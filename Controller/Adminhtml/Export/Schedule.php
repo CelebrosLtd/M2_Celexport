810786d8fa83cc8c317f1d5b1358e928facd36e9
Devbelvg <devbelvg@gmail.com>
2020-06-30 15:56:00 +0000

    autoschedule after image refresh added

--- Controller/Adminhtml/Export/Schedule.php
+++ Controller/Adminhtml/Export/Schedule.php
@@ -13,92 +13,47 @@
  */
 namespace Celebros\Celexport\Controller\Adminhtml\Export;
 
+use Celebros\Celexport\Helper\Cron as Scheduler;
+
 class Schedule extends \Celebros\Celexport\Controller\Adminhtml\Export
 {
-    const CRON_GROUP = 'default';
-    const CRON_JOB = 'celebros_export';
-    
     /**
-     * @var \Magento\Cron\Model\Config
+     * @var \Celebros\Celexport\Helper\Cron
      */
-    protected $_cronConfig;
-    
-    /**
-     * @var \Magento\Cron\Model\Schedule
-     */
-    protected $_cronSchedule;
-    
-    /**
-     * @var \Magento\Framework\Stdlib\DateTime\TimezoneInterface
-     */
-    protected $_timezone;
+    protected $scheduler;
     
     /**
      * @param \Magento\Backend\App\Action\Context $context
-     * @param \Magento\Cron\Model\Config $cronConfig
+     * @param \Celebros\Celexport\Helper\Cron $scheduler
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Cron\Model\Config $cronConfig
+        Scheduler $scheduler
     ) {
         parent::__construct($context);
-        $this->_cronConfig = $cronConfig;
-        $this->_timezone = $this->_objectManager->create('Magento\Framework\Stdlib\DateTime\TimezoneInterface');
-        $this->_cronSchedule = $this->_objectManager->create('Magento\Cron\Model\Schedule');
+        $this->scheduler = $scheduler;
     }
     
     public function execute()
     {
         if (!$this->_isAllowed()) {
-            $this->getResponse()->setBody($this->__('Access Denied'));
-            return;
-        }
-        
-        $SCHEDULE_EVERY_MINUTES = 30;
-        //Flooring the minutes
-        $startTimeSeconds = ((int)($this->_timezone->scopeTimeStamp()/60)) * 60;
-        //Ceiling to the next 5 minutes
-        $startTimeMinutes = $startTimeSeconds / 60;
-        $startTimeMinutes = ((int)($startTimeMinutes / 5)) * 5 + 5;
-        $startTimeSeconds = $startTimeMinutes * 60;
-        
-        $jobs = $this->_cronConfig->getJobs();
-        
-        if (isset($jobs[self::CRON_GROUP])) {
-            $i = 0;
-            foreach ($jobs[self::CRON_GROUP] as $jobCode => $jobConfig) {
-                if (strpos($jobCode, self::CRON_JOB) === false) {
-                    continue;
-                }
-                
-                $timecreated   = strftime('%Y-%m-%d %H:%M:%S', $this->_timezone->scopeTimeStamp());
-                $timescheduled = strftime('%Y-%m-%d %H:%M:%S', $startTimeSeconds + $i * 60 * $SCHEDULE_EVERY_MINUTES);
-                try {
-                    $lastItem = $this->_cronSchedule->getCollection()
-                        ->addFieldToFilter('job_code', 'celebros_export')
-                        ->addFieldToFilter('scheduled_at', $timescheduled)
-                        ->getLastItem();
-                     
-                    if (!$lastItem->getScheduleId()) {
-                        $this->_cronSchedule->setJobCode($jobCode)
-                        ->setCreatedAt($timecreated)
-                        ->setScheduledAt($timescheduled)
-                        ->setStatus('pending')
-                        ->save();
-                        
-                        $body = "{$jobCode} cron job is scheduled at $timescheduled <br/>";
-                    } else {
-                        $body = "{$jobCode} cron job are already exist at $timescheduled <br/>";
-                    }
-                } catch (\Exception $e) {
-                    throw new \Exception(__('Unable to schedule Cron'));
-                }
-                
-                $i++;
+            $this->messageManager->addError(
+                __('Access Denied')
+            );
+        } else {
+            if ($timescheduled = $this->scheduler->scheduleNewExport()) {
+                $body = __("Celebros export cron job is scheduled at $timescheduled <br/>");
+                $this->messageManager->addSuccess(
+                    __("Celebros export cron job is scheduled at $timescheduled <br/>")
+                );
+            } else {
+                $this->messageManager->addWarning(
+                    __("Celebros export cron job are already exist <br/>")
+                );
             }
         }
         
-        $this->getResponse()->setBody($body);
+        $this->_redirect($this->_redirect->getRefererUrl());
     }
     
     /**
