3fa9327347924c3569c4615bd12f2c52f355e9be
Devbelvg <devbelvg@gmail.com>
2020-06-30 15:27:51 +0000

    config endpoints added to api

--- Helper/Data.php
+++ Helper/Data.php
@@ -24,6 +24,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     const CONFIG_EXPORT_ORDERS_FILENAME = 'celexport/export_settings/datahistoryname';
     const CONFIG_EXPORT_CHUNK_SIZE = 'celexport/advanced/export_chunk_size';
     const CONFIG_EXPORT_PROCESS_LIMIT = 'celexport/advanced/export_process_limit';
+    const CONFIG_EXPORT_LOG = 'celexport/advanced/enable_log';
     const CONFIG_EXPORT_INDEXED_PRICES = 'celexport/export_settings/indexed_prices';
     const CONFIG_EXPORT_USE_CATALOG_PRICE_RULES = 'celexport/export_settings/use_catalog_price_rules';
     const CONFIG_EXPORT_CONF_ENV_STAMP = 'celexport/ftp_prod/env_stamp';
@@ -40,18 +41,22 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     protected $_dir;
     protected $_assetRepo;
     protected $_resource;
+    protected $_configWriter;
     protected $dirWrite;
     protected $_body = null;
     
     /**
-     * @param \Magento\Framework\App\Helper\Context $context,
-     * @param \Magento\Framework\Code\Minifier\Adapter\Css\CSSmin $cssMin,
-     * @param \Magento\Framework\Code\Minifier\Adapter\Js\JShrink $jsMin,
-     * @param \Magento\Store\Model\StoreManager $stores,
-     * @param \Magento\Framework\View\Asset\Repository $assetRepo,
-     * @param \Magento\Framework\Filesystem\DirectoryList $dir,
-     * @param \Magento\Framework\Filesystem $filesystem,
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\Code\Minifier\Adapter\Css\CSSmin $cssMin
+     * @param \Magento\Framework\Code\Minifier\Adapter\Js\JShrink $jsMin
+     * @param \Magento\Framework\App\ResponseInterface $response
+     * @param \Magento\Store\Model\StoreManager $stores
+     * @param \Magento\Framework\View\Asset\Repository $assetRepo
+     * @param \Magento\Framework\Filesystem\DirectoryList $dir
+     * @param \Magento\Framework\Filesystem $filesystem
      * @param \Magento\Framework\App\ResourceConnection $resource
+     * @param \Magento\Config\Model\ResourceModel\Config $resourceConfig
+     * @return void
      */
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
@@ -62,7 +67,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         \Magento\Framework\View\Asset\Repository $assetRepo,
         \Magento\Framework\Filesystem\DirectoryList $dir,
         \Magento\Framework\Filesystem $filesystem,
-        \Magento\Framework\App\ResourceConnection $resource
+        \Magento\Framework\App\ResourceConnection $resource,
+        \Magento\Config\Model\ResourceModel\Config $resourceConfig
     ) {
         $this->_urlBuilder = $context->getUrlBuilder();
         $this->_cssMin = $cssMin;
@@ -72,8 +78,116 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         $this->_dir = $dir;
         $this->_assetRepo = $assetRepo;
         $this->_resource = $resource;
+        $this->resourceConfig = $resourceConfig;
         $this->_log = $filesystem->getDirectoryWrite($dir::ROOT);
         parent::__construct($context);
+    }
+    
+    /**
+     * Extract list of all export settings for store
+     *
+     * @param int $store
+     * @return array
+     */
+    public function getAllSettings($store = null) : array
+    {
+        $result = [];
+        foreach ($this->getAllConfigPaths() as $name => $path) {
+            $result[$name] = $this->getConfig(
+                $path,
+                $store
+            );
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Get list of available config paths
+     *
+     * @return array
+     */
+    protected function getAllConfigPaths() : array
+    {
+        $result = [];
+        $reflectionClass = new \ReflectionClass($this);
+        $configPaths = $reflectionClass->getConstants();
+        foreach ($configPaths as $name => $path) {
+            if (strpos($name, 'CONFIG_') !== false) {
+                $result[$name] = $path;
+            }
+        }
+        
+        return $result;
+    }
+    
+    public function getAllStores()
+    {
+        return $this->_stores->getStores();
+    }
+    
+    public function getCurrentStore()
+    {
+        return $this->_stores->getStore();
+    }
+    
+    public function setCurrentStore($storeId)
+    {
+        try {
+            $this->_stores->setCurrentStore($storeId);
+        } catch (\Exception $e) {
+            $this->logProfiler($e->getMessage());
+        }
+    }
+    
+    public function getConfig(string $path, $store = null, $isBool = false)
+    {
+        if ($store) {
+            $store = $this->getCurrentStore();
+        }
+        
+        if ($isBool) {
+            return $this->scopeConfig->isSetFlag(
+                $path,
+                ScopeInterface::SCOPE_STORES,
+                $store
+            );
+        }
+        
+        return $this->scopeConfig->getValue(
+            $path,
+            ScopeInterface::SCOPE_STORES,
+            $store
+        );
+    }
+    
+    public function setConfig(string $name, $value, $store = null)
+    {
+        if ($path = $this->_validateConfigPath($name)) {
+            $store = ($store !== null) ? $store : 0;
+            $this->resourceConfig->saveConfig(
+                $path,
+                $value,
+                $store ? ScopeInterface::SCOPE_STORES : \Magento\Framework\App\ScopeInterface::SCOPE_DEFAULT,
+                $store
+            );
+            
+            return $this->getConfig(
+                $path,
+                $store
+            );
+        }
+        
+        return null;
+    }
+    
+    protected function _validateConfigPath(string $name) : ?string
+    {
+        $validPaths = $this->getAllConfigPaths();
+        if (isset($validPaths[$name])) {
+            return $validPaths[$name];
+        }
+        return null;
     }
     
     public function isEnabled($store = null)
@@ -82,7 +196,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             $store = $this->getCurrentStore();
         }
         
-        return (bool)$this->scopeConfig->getValue(self::CONFIG_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store);
+        return (bool)$this->scopeConfig->getValue(self::CONFIG_ENABLED, \Magento\Store\Model\ScopeInterface::SCOPE_STORES, $store);
     }
     
     public function isOrdersExport($store = null)
@@ -91,7 +205,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             $store = $this->getCurrentStore();
         }
         
-        return (bool)$this->scopeConfig->getValue(self::CONFIG_EXPORT_ORDERS, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store);
+        return (bool)$this->scopeConfig->getValue(self::CONFIG_EXPORT_ORDERS, \Magento\Store\Model\ScopeInterface::SCOPE_STORES, $store);
     }
     
     public function getDataHistoryFileName($store = null)
@@ -100,7 +214,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             $store = $this->getCurrentStore();
         }
         
-        return $this->scopeConfig->getValue(self::CONFIG_EXPORT_ORDERS_FILENAME, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store);
+        return $this->scopeConfig->getValue(self::CONFIG_EXPORT_ORDERS_FILENAME, \Magento\Store\Model\ScopeInterface::SCOPE_STORES, $store);
     }
     
     public function getCurrentEnvStamp()
@@ -110,39 +224,12 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     
     public function getConfiguratedEnvStamp()
     {
-        return $this->scopeConfig->getValue(self::CONFIG_EXPORT_CONF_ENV_STAMP, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return $this->scopeConfig->getValue(self::CONFIG_EXPORT_CONF_ENV_STAMP, \Magento\Store\Model\ScopeInterface::SCOPE_STORES);
     }
     
     public function getExportLifetime()
     {
         return (int)$this->scopeConfig->getValue(self::CONFIG_EXPORT_LIFETIME);
-    }
-    
-    public function getAllStores()
-    {
-        return $this->_stores->getStores();
-    }
-    
-    public function setCurrentStore($storeId)
-    {
-        try {
-            $this->_stores->setCurrentStore($storeId);
-        } catch (\Exception $e) {
-        }
-    }
-    
-    public function getCurrentStore()
-    {
-        return $this->_stores->getStore();
-    }
-    
-    public function getConfig($path, $store = null)
-    {
-        if ($store) {
-            $store = $this->getCurrentStore();
-        }
-        
-        return $this->scopeConfig->getValue($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store);
     }
     
     public function getExportPath($id = null)
@@ -171,7 +258,7 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             $store = $this->getCurrentStore();
         }
         
-        return $this->scopeConfig->getValue(self::CONFIG_NOTIFICATION_EMAIL, \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store);    
+        return $this->scopeConfig->getValue(self::CONFIG_NOTIFICATION_EMAIL, \Magento\Store\Model\ScopeInterface::SCOPE_STORES, $store);
     }
     
     public function useIndexedPrices()
@@ -184,9 +271,23 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         return (int)$this->scopeConfig->getValue(self::CONFIG_EXPORT_USE_CATALOG_PRICE_RULES);
     }
     
+    public function getCronlogLifetime()
+    {
+        return (int)$this->scopeConfig->getValue(self::CONFIG_CRON_LOG_LIFETIME);
+    }
+    
+    public function isCustomAttributesEnabled($store = null)
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::CONFIG_CUSTOM_ATTRIBUTES,
+            ScopeInterface::SCOPE_STORES,
+            $store
+        );
+    }
+    
     public function logProfiler($msg, $process = null)
     {
-        if (!$this->scopeConfig->getValue('celexport/advanced/enable_log')) {
+        if (!$this->scopeConfig->getValue(self::CONFIG_EXPORT_LOG)) {
             return;
         }
         
@@ -253,19 +354,5 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     public function getIconUrl($file)
     {
         return $this->_assetRepo->getUrl('Celebros_Celexport::images/' . $file);
-    }
-    
-    public function getCronlogLifetime()
-    {
-        return (int)$this->scopeConfig->getValue(self::CONFIG_CRON_LOG_LIFETIME);
-    }
-    
-    public function isCustomAttributesEnabled($store = null)
-    {
-        return $this->scopeConfig->isSetFlag(
-            self::CONFIG_CUSTOM_ATTRIBUTES,
-            ScopeInterface::SCOPE_STORE,
-            $store
-        );
     }
 }
--- Model/ExportManagement.php
+++ Model/ExportManagement.php
@@ -15,47 +15,75 @@ namespace Celebros\Celexport\Model;
 
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
+use Magento\Framework\App\Filesystem\DirectoryList;
 
 class ExportManagement implements \Celebros\Celexport\Api\ExportManagementInterface
 {
-    protected $_storeId;
+    const NOT_EXIST = "not_exist";
+    const STARTED = "started";
+    const IN_PROGRESS = "in_progress";
+    const DONE = "done";
+    const FAILED = "failed";
+    const ERROR = "error";
     
+    const MAX_PROCESS_AGE = 7200; /* 2h */
+    const MAX_LAST_UPDATE_AGE = 3600; /* 1h */
+    
+    /**
+     * @var int
+     */
+    protected $id;
+    
+    /**
+     * @var string
+     */
+    protected $status;
+    
+    /**
+     * @param \Celebros\Celexport\Model\Exporter $exporter
+     * @param \Celebros\Celexport\Helper\Data $helper
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Filesystem $filesystem
+     * @param \Magento\Framework\Filesystem\DirectoryList $dir
+     * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone
+     * @param \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
+     * @return void
+     */
     public function __construct(
-        \Celebros\Celexport\Model\Exporter $celebrosExport,
-        \Celebros\Celexport\Helper\Data $celebrosHelper,
-        \Magento\Sales\Model\Order\Item $ordeModel,
-        \Magento\Framework\App\RequestInterface $request,
+        \Celebros\Celexport\Model\Exporter $exporter,
+        \Celebros\Celexport\Helper\Data $helper,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Framework\Filesystem $filesystem,
         \Magento\Framework\Filesystem\DirectoryList $dir,
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $timezone,
         \Magento\Framework\Mail\Template\TransportBuilder $transportBuilder
     ) {
-        $this->celebrosExport = $celebrosExport;
-        $this->helper = $celebrosHelper;
-        $this->orderModel = $ordeModel;
-        $this->request = $request;
+        $this->exporter = $exporter;
+        $this->helper = $helper;
         $this->storeManager = $storeManager;
-        $this->_filesystem = $filesystem;
-        $this->_dir = $dir;
+        $this->filesystem = $filesystem;
+        $this->dir = $dir;
+        $this->timezone = $timezone;
         $this->transportBuilder = $transportBuilder;
     }
     
     public function getIntMediaFolder($folder)
     {
-        return $this->_filesystem->getDirectoryRead(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA)->getAbsolutePath() . $folder;
-    }    
+        return $this->filesystem->getDirectoryRead(DirectoryList::MEDIA)->getAbsolutePath() . $folder;
+    }
     
-    public function exportData($dataType, int $storeId)
+    public function exportData($dataType, int $id)
     {
-        $this->_storeId = $storeId;
-        $methodName = 'getData_' . $dataType;
+        $this->id = $id;
+        $methodName = 'getData' . str_replace("_", "", ucwords($dataType, "_"));
         if (method_exists($this, $methodName)) {
             return $this->$methodName();
         }
         
-        return [
+        return ['response' => [
+            'status' => self::ERROR,
             'message' => 'data type is not exist'
-        ];
+        ]];
     }
     
     protected function _prepareExportFolder()
@@ -64,7 +92,7 @@ class ExportManagement implements \Celebros\Celexport\Api\ExportManagementInterf
             mkdir($this->getIntMediaFolder('celebros'), 0777);
         }
         
-        array_map('unlink', array_filter((array) glob($this->getIntMediaFolder('celebros') . "/*")));   
+        array_map('unlink', array_filter((array) glob($this->getIntMediaFolder('celebros') . "/*")));
     }
     
     protected function _moveAndRenameExportZip($path)
@@ -76,61 +104,122 @@ class ExportManagement implements \Celebros\Celexport\Api\ExportManagementInterf
         return $this->convertPathToUrl($this->getIntMediaFolder('celebros') . "/" . $name . ".zip");
     }
     
-    public function getData_start_export()
+    public function getDataStartExport()
     {
-        $storeId = $this->_storeId ? : 1;
-        $comm = 'nohup php ' . $this->_dir->getRoot() . '/bin/magento celebros:export ' . $storeId .' > /dev/null & echo $!';
+        $storeId = $this->id ? : 1;
+        $exportProcessId = (int)$this->helper->getExportProcessId();
+        $comm = 'nohup php ' . $this->dir->getRoot() . '/bin/magento celebros:export ' . $storeId . ' ' . $exportProcessId . ' > /dev/null & echo $!';
         $process = new Process($comm);
         $process->start();
         $pid = $process->getPid();
         
-        return [
-            'pid' => $pid
-        ];
+        return ['response' => [
+            'status' => self::STARTED,
+            'pid' => $pid,
+            'export_process_id' => $exportProcessId
+        ]];
     }
     
     public function convertPathToUrl($path)
     {
-        $rootPath = $this->_dir->getRoot();
+        $rootPath = $this->dir->getRoot();
         $baseUrl = $this->storeManager->getStore()->getBaseUrl();
         
-        return str_replace($this->_dir->getRoot(), $this->storeManager->getStore()->getBaseUrl(), $path);
-    }   
-
-    public function getData_is_export_ready()
+        return str_replace($this->dir->getRoot(), $this->storeManager->getStore()->getBaseUrl(), $path);
+    }
+    
+    public function getExportStatusByStore($store)
     {
-        $pid = $this->_storeId;
-        if (is_array(glob($this->getIntMediaFolder('celebros') . "/*.zip"))) {
-            foreach (glob($this->getIntMediaFolder('celebros') . "/*.zip") as $filename) {
-                return [
-                    'result' => 'success',
-                    'file' => $filename
-                ];
+        $result = [];
+        $storeStatus = self::STARTED;
+        $folder = $this->helper->getExportPath($this->id) . '/' . $store->getWebsite()->getCode() . '/' . $store->getCode();
+        $fileInfo = new \SplFileInfo($this->helper->getExportPath($this->id));
+        $startedAt = $fileInfo->getCTime();
+        $updates = [];
+        $dir = new \DirectoryIterator($folder);
+                
+        foreach ($dir as $fileInfo) {
+            if (!$fileInfo->isDir() && !$fileInfo->isDot()) {
+                $this->status = self::DONE;
+                if ($fileInfo->getExtension() == 'zip') {
+                    $result['zip'][] =  $fileInfo->getFilename();
+                    $storeStatus = ($storeStatus !== self::IN_PROGRESS) ? self::DONE : $storeStatus;
+                } else {
+                    $result['files'][] =  $fileInfo->getFilename();
+                    $storeStatus = self::IN_PROGRESS;
+                }
+                $updates[] = $fileInfo->getCTime();
             }
         }
         
-        if ($this->is_process_running($pid)) {
-            return [
-                'result' => 'in_progress'
-            ];
+        if ($storeStatus !== self::DONE) {
+            if (!empty($updates) && (max($updates) + self::MAX_LAST_UPDATE_AGE) <= time()
+            || ($startedAt + self::MAX_PROCESS_AGE) <= time()) {
+                $this->status = $storeStatus = self::FAILED;
+            } elseif ($storeStatus === self::IN_PROGRESS) {
+                $this->status = self::IN_PROGRESS;
+            }
         }
-      
+        
         return [
-            'result' => 'failed'
+            'started_at' => $this->_prepareDateTime($startedAt),
+            'last_updated_at' => !empty($updates) ? $this->_prepareDateTime(max($updates)) : $this->_prepareDateTime($startedAt),
+            'status' => $storeStatus,
+            'files' => $result
         ];
     }
     
-    public function startExportProcess($storeId = null)
+    protected function _prepareDateTime(string $ts = null)
+    {
+        $ts = $ts ? : time();
+        return  $this->timezone->date($ts)
+            ->format(\Magento\Framework\Stdlib\DateTime::DATETIME_PHP_FORMAT);
+    }
+    
+    protected function _isProcessStarted()
+    {
+        try {
+            $fileInfo = new \SplFileInfo($this->helper->getExportPath($this->id));
+            if ($fileInfo->isDir()) {
+                return true;
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
+        
+        return false;
+    }
+    
+    public function getDataCheckStatus()
+    {
+        $this->status = self::NOT_EXIST;
+        
+        if ($this->_isProcessStarted()) {
+            $this->status = self::STARTED;
+            $stores = $this->helper->getAllStores();
+            foreach ($stores as $store) {
+                $result['stores'][$store->getWebsite()->getCode() . "/" . $store->getCode()] = $this->getExportStatusByStore($store);
+            }
+        }
+        
+        $result['status'] = $this->status;
+        return ['response' => $result];
+    }
+    
+    public function startExportProcess(int $storeId = null, int $exportProcessId = null)
     {
         try {
             $this->_prepareExportFolder();
             $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-            $this->celebrosExport->export_celebros($objectManager, false, $storeId);
-            $url = $this->celebrosExport->zipFileName ? : '';
+            if ($exportProcessId) {
+                $this->exporter->setExportProcessId($exportProcessId);
+            }
+            $this->exporter->export_celebros($objectManager, false, $storeId);
+            $url = $this->exporter->zipFileName ? : '';
             if ($url) {
                 return ['export_url' => $this->_moveAndRenameExportZip($url)];
             } else {
-                return [];   
+                return [];
             }
         } catch (\Exception $e) {
             $this->sendNotificationToEmail($e->getMessage());
@@ -141,7 +230,8 @@ class ExportManagement implements \Celebros\Celexport\Api\ExportManagementInterf
     {
         $store = $this->storeManager->getStore()->getId();
         $notificationEmail = $this->helper->getNotificationsEmail($store);
-        $transport = $this->transportBuilder->setTemplateIdentifier('celexport_notification_email_template')
+        $transport = $this->transportBuilder
+            ->setTemplateIdentifier('celexport_notification_email_template')
             ->setTemplateOptions(['area' => 'frontend', 'store' => $store])
             ->setTemplateVars(
                 [
@@ -157,6 +247,5 @@ class ExportManagement implements \Celebros\Celexport\Api\ExportManagementInterf
         $transport->sendMessage();
         
         return $this;
-        
     }
 }
